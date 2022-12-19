329e31c26dfd13d648491158bdf2fce7a7120b10
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-12-18 21:12:20 -0500

    wip

--- src/kaocha/api.clj
+++ src/kaocha/api.clj
@@ -103,6 +103,8 @@
                 (when-not (some #(or (hierarchy/leaf? %)
                                      (::testable/load-error %))
                                 (testable/test-seq test-plan))
+                  (output/warn (format "Testable seq %s" (testable/test-seq test-plan)))
+                  (output/warn (format "Test plan: %s" test-plan))
                   (if (not (zero? (count (filter ::testable/skip (testable/test-seq-with-skipped test-plan)))))
                     (output/warn (format (str "All %d tests were skipped."
                                               " Check for misspelled settings in your Kaocha test configuration"
