e3ed41b2ec6b459f27ec211d91f1ef1df9e0b399
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-11-19 18:06:09 -0600

    [skip ci]

--- src/kaocha/plugin/profiling/combine_results.clj
+++ src/kaocha/plugin/profiling/combine_results.clj
@@ -1,10 +1,13 @@
 #!/usr/bin/env bb
+;;./src/kaocha/plugin/profiling/combine_results.clj '{:result-files ["profiling.edn"] :output-file "profiling1.edn"}'
 (ns kaocha.plugin.profiling.combine-results
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [babashka.fs :as fs]))
 
 ;; ./bin/kaocha --write-profiling-file profiling.edn --read-profiling-file profiling.edn --focus kaocha.plugin.filter-test --partition-index 1 --partitions 5 --partition-strategy :var-time --seed 0 --target-partition-minutes 5
 (defn combine-results [result-files]
-  (let [forms (mapv (comp edn/read-string slurp) result-files)
+  (let [forms (mapv (comp edn/read-string slurp fs/file)
+                    (mapcat #(fs/glob "." %) result-files))
         _ (assert (seq forms))
         results (mapv :results forms)
         except-results (mapv #(dissoc % :results) forms)
@@ -12,18 +15,18 @@
         _ (assert (every? #(= 1 (:kaocha.plugin.profiling/version %)) forms))
         base (assoc (first except-results)
                     :results (apply merge-with #(merge-with into %1 %2) (map :results forms)))
-        {:keys [partition-target-minutes max-partitions]} (get-in base [:kaocha/cli-options])]
+        {:keys [target-partition-minutes max-partitions]} (:kaocha/cli-options base)]
     (cond-> base
-      partition-target-minutes (assoc :suggested-partitions
+      target-partition-minutes (assoc :suggested-partitions
                                       (let [max-partitions (or max-partitions 10)
                                             _ (assert (pos? max-partitions))
-                                            partition-target-ns (*' 60 60 1e6 partition-target-minutes)
+                                            target-partition-ns (*' 60 60 1e6 target-partition-minutes)
                                             total-duration-ns (apply +' (map :kaocha.plugin.profiling/duration
                                                                              (vals (get-in base [:results :kaocha.type/var]))))]
                                         ;; 100ns total duration
                                         ;; 10ns target
                                         ;; 100/10 => 10 partitions
-                                        (-> (/ total-duration-ns partition-target-ns)
+                                        (-> (/ total-duration-ns target-partition-ns)
                                             Math/ceil
                                             (max max-partitions)
                                             (min 1)))))))
