594915ceb70d1c749e0b8b3115af4cfe79fe57dc
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-11-27 19:13:08 -0600

    dbg

--- src/kaocha/plugin/profiling/suggested_partitions.clj
+++ src/kaocha/plugin/profiling/suggested_partitions.clj
@@ -7,6 +7,9 @@
             [cheshire.core :as json]
             [babashka.fs :as fs]))
 
+(defn- dbg [{:keys [debug] :as m} msg]
+  (some-> debug (spit msg :append true)))
+
 (defn suggest-partitions
   "Returns a suggested number of test partitions based on previous results."
   [{:keys [input-file default-partitions max-partitions]}]
@@ -18,18 +21,19 @@
     (min (or suggested-partitions default-partitions)
          max-partitions)))
 
-(defn- set-github-actions-output [{:github-actions/keys [set-matrix-output debug] :as m} npartitions]
+(defn- set-github-actions-output [{:github-actions/keys [set-matrix-output] :as m} npartitions]
   (when set-matrix-output
     (-> npartitions range json/encode
         (as-> $ (spit (System/getenv "GITHUB_OUTPUT")
                       (let [delim (random-uuid)
                             s (format "%s<<%s\n%s\n%s\n" set-matrix-output delim $ delim)]
-                        (some-> debug (spit s :append true))
+                        (dbg m s)
                         s)
                       :append true)))))
 
 (defn run [m]
   (let [npartitions (suggest-partitions m)]
+    (dbg m (format "npartitions=%s" npartitions))
     (set-github-actions-output m npartitions)
     (println
       (case (:print m :partitions)
